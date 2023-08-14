2bdbbb48ed8a0123790032eae93f92d0f5420508
Gary Ash <gary.ash@icloud.com>
2023-08-14 13:44:35 -0400

    Added tooltips to the navigation bar menu.

--- scripts/mode-switcher.js
+++ scripts/mode-switcher.js
@@ -2,19 +2,19 @@ let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
 let theme = sessionStorage.getItem('theme');
 
 if (systemInitiatedDark.matches) {
-	document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-sun\"></i>";
+	document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-solid fa-lightbulb\"></i>";
 } else {
-	document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-moon\"></i>";
+	document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-lightbulb\"></i>";
 }
 
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
   	document.documentElement.setAttribute('data-theme', 'dark');		
-   	document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-sun\"></i>";
+   	document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-solid fa-lightbulb\"></i>";
    	sessionStorage.setItem('theme', '');
   } else {
   	document.documentElement.setAttribute('data-theme', 'light');
-    document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-moon\"></i>";
+    document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-lightbulb\"></i>";
     sessionStorage.setItem('theme', '');
   }
 }
@@ -26,28 +26,28 @@ function modeSwitcher() {
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-moon\"></i>";
+		document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-lightbulb\"></i>";
 	}	else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-sun\"></i>";
+		document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-solid fa-lightbulb\"></i>";
 	} else if (systemInitiatedDark.matches) {	
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-moon\"></i>";
+		document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-lightbulb\"></i>";
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-sun\"></i>";
+		document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-solid fa-lightbulb\"></i>";
 	}
 }
 
 if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
-	document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-sun\"></i>";
+	document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-solid fa-lightbulb\"></i>";
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
-	document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-moon\"></i>";
+	document.getElementById("theme-toggle").innerHTML = "<i class=\"fa-regular fa-lightbulb\"></i>";
 }
